f0578440e5cfe93d772aa334627ebe0f2911ec88
grbzemir <eyetkingurbuz@trakya.edu.tr>
2024-09-02 14:45:03 +0300

    Navbar

--- Restoran/src/App.jsx
+++ Restoran/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import Navbar from './Components/Navbar'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './Pages/Home';
 
 function App() {
 
@@ -11,7 +12,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route path='/' element={Home}>
+          <Route path='/' element={<Home />} />
         </Routes>
       </Router>
 
---
+++ Restoran/src/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div>
+            Welcome To Home Page
+        </div>
+    )
+}
+
+export default Home
